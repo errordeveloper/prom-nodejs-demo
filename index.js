eecb6a258986919c81b99f6a76e3f0b5dc2a0aa3
Ilya Dmitrichenko <errordeveloper@gmail.com>
2017-06-16 13:40:12 +0100

    Switch to epimetheus, fix typo

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 const restify = require('restify');
-const promBundle = require('restify-prom-bundle');
+const epimetheus = require('epimetheus');
 
 const port = 8080;
 
 const server = restify.createServer();
 
-server.pre(promBundle.preMiddleware(server, {}));
+epimetheus.instrument(server);
 
 server.get('/', (req, res, done) => {
-  res.send('Hello, friend! We are in stelth mode... See you later.');
+  res.send('Hello, friend! We are in stealth mode... See you later.');
   done();
 });
 
 server.listen(port, () => {
   console.log(`server is listening on port ${port}`);
 });
-
