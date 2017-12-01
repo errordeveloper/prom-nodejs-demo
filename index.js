5861e189b8747d67d4c55beafce65a67a465ded6
Ilya Dmitrichenko <errordeveloper@gmail.com>
2017-12-01 11:39:40 +0000

    Stringly FTW!

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
 const http = require('http');
+const url = require('url');
+
 const port = 8080;
+
+const welcomeToStringlyApp = (frontendHostname) => {
+  return `
+    Welcome to stringly app!
+
+    Here is some basic documentation to help you get started:
+
+    > curl -s "http://${frontendHostname}/reverse?string=k8s"
+    s8k
+
+    Have fun using stringly app :-)
+  `
+}
+
+const millionDollarStringlyAlgorithm = (payloadString) => {
+  return payloadString.split('').reverse().join('');
+}
 
 const requestHandler = (request, response) => {
   console.log(request.url);
-  response.end(`Hello, World!`);
+  const u = url.parse(request.url, true);
+
+  if (u.pathname === '/reverse' && u.query.string !== undefined) {
+    response.end(millionDollarStringlyAlgorithm(u.query.string));
+  } else {
+    response.end(welcomeToStringlyApp(request.headers.host));
+  }
 }
 
 const server = http.createServer(requestHandler);
