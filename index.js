bce053f2ecd122d5f90a0ad068ebb8dec3c37e88
Ilya Dmitrichenko <errordeveloper@gmail.com>
2017-12-03 05:45:46 +0000

    Use stringly app

--- index.js
+++ index.js
@@ -7,8 +7,37 @@ const server = restify.createServer();
 
 epimetheus.instrument(server);
 
+server.use(restify.queryParser());
+server.use(restify.requestLogger());
+
+function welcomeToStringlyApp (frontendHostname) {
+  return `
+    Welcome to Stringly™ app!
+
+    Here is some basic documentation to help you get started:
+
+    > curl "http://${frontendHostname}/reverse?string=banana"
+    ananab
+
+    Have fun using Stringly™ app :-)
+  `
+}
+
+function millionDollarStringlyAlgorithm (payloadString) {
+  return payloadString.split('').reverse().join('');
+}
+
 server.get('/', (req, res, done) => {
-  res.send('Hello, friend! We are still in stealth mode... See you later.');
+  console.log(req);
+  res.header('content-type', 'text/plain')
+  res.send(welcomeToStringlyApp(req.headers.host));
+  done();
+});
+
+server.get('/reverse', (req, res, done) => {
+  res.header('content-type', 'text/plain');
+  // TODO: use a counter for string length and timer for how long it takes to reverse
+  res.send(millionDollarStringlyAlgorithm(req.params['string']));
   done();
 });
 
